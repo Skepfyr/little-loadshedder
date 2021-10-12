32917bd44c1f7a39e712d03d9289d1056cf58ecf
Jack Rickard <jack.rickard@metaswitch.com>
2021-10-12 20:13:24 +0100

    Calculate average latency

---
+++ examples/microservice.rs
@@ -0,0 +1,27 @@
+use std::{convert::Infallible, net::SocketAddr, time::Duration};
+
+use hyper::{service::service_fn, Body, Request, Response, Server};
+use tower::{make::Shared, ServiceBuilder};
+use underload::LoadShedLayer;
+
+#[tokio::main]
+async fn main() {
+    // We'll bind to 127.0.0.1:3000
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+
+    let service = ServiceBuilder::new()
+        .layer(LoadShedLayer::new(0.2, Duration::from_millis(100)))
+        .service(service_fn(hello_world));
+
+    let server = Server::bind(&addr).serve(Shared::new(service));
+
+    // Run this server for... forever!
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
+    }
+}
+
+async fn hello_world(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    tokio::time::sleep(Duration::from_millis(10)).await;
+    Ok(Response::new("Hello, World".into()))
+}
---
+++ src/lib.rs
@@ -0,0 +1,114 @@
+use std::{
+    future::Future,
+    pin::Pin,
+    sync::{Arc, Mutex},
+    task::{Context, Poll},
+    time::{Duration, Instant},
+};
+
+use tower::{Layer, Service};
+
+pub struct LoadShedLayer {
+    p95: Duration,
+    /// Seconds
+    moving_average: Arc<Mutex<f64>>,
+    /// In the range (0, 1)
+    /// .25 means new values account for 25% of the moving average
+    ewma_param: f64,
+}
+
+impl LoadShedLayer {
+    pub fn new(ewma_param: f64, p95: Duration) -> Self {
+        Self {
+            p95,
+            moving_average: Arc::new(Mutex::new(p95.as_secs_f64())),
+            ewma_param,
+        }
+    }
+}
+
+impl<Service> Layer<Service> for LoadShedLayer {
+    type Service = LoadShed<Service>;
+
+    fn layer(&self, inner: Service) -> Self::Service {
+        LoadShed {
+            inner,
+            p95: self.p95,
+            moving_average: Arc::clone(&self.moving_average),
+            ewma_param: self.ewma_param,
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct LoadShed<Inner> {
+    inner: Inner,
+    p95: Duration,
+    /// Seconds
+    moving_average: Arc<Mutex<f64>>,
+    /// In the range (0, 1)
+    /// .25 means new values account for 25% of the moving average
+    ewma_param: f64,
+}
+
+impl<Request, Inner> Service<Request> for LoadShed<Inner>
+where
+    Inner: Service<Request>,
+    Inner::Response: std::fmt::Debug,
+    Inner::Error: std::fmt::Debug,
+{
+    type Response = Inner::Response;
+    type Error = Inner::Error;
+    type Future = LoadShedFut<Inner::Future>;
+
+    fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        self.inner.poll_ready(cx)
+    }
+
+    fn call(&mut self, req: Request) -> Self::Future {
+        LoadShedFut {
+            inner: self.inner.call(req),
+            start: Instant::now(),
+            ewma_param: self.ewma_param,
+            moving_average: Arc::clone(&self.moving_average),
+        }
+    }
+}
+
+#[pin_project::pin_project]
+pub struct LoadShedFut<Inner> {
+    #[pin]
+    inner: Inner,
+    start: Instant,
+    ewma_param: f64,
+    moving_average: Arc<Mutex<f64>>,
+}
+
+impl<Inner> Future for LoadShedFut<Inner>
+where
+    Inner: Future,
+    Inner::Output: std::fmt::Debug,
+{
+    type Output = Inner::Output;
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        
+        let this = self.project();
+        let result = match this.inner.poll(cx) {
+            Poll::Ready(result) => result,
+            Poll::Pending => return Poll::Pending,
+        };
+
+        let elapsed: f64 = this.start.elapsed().as_secs_f64();
+        println!("Elapsed time for request is {}s", elapsed);
+        let moving_average = {
+            let mut moving_average = this.moving_average.lock().unwrap();
+            *moving_average =
+                (*moving_average * (1.0 - *this.ewma_param)) + (*this.ewma_param * elapsed);
+            *moving_average
+        };
+        println!("New average: {}", moving_average);
+
+        Poll::Ready(result)
+    }
+}
