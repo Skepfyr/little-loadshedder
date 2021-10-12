a2b4a147e0066a01efcab0be19a1bd4238aae840
Nathaniel Tracy-Amoroso <nathanielt@microsoft.com>
2021-10-12 16:02:39 -0700

    configurable max_inflight variable that drops requests when max_inflight already in flight, does not adjust this variable yet

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ use std::{
     task::{Context, Poll},
     time::{Duration, Instant},
 };
+use thiserror::Error;
 
 use tower::Service;
 
@@ -17,10 +18,24 @@ struct LoadShedConf {
     /// In the range (0, 1)
     /// .25 means new values account for 25% of the moving average
     ewma_param: f64,
-    /// Max inflight requests
-    inflight: Arc<Mutex<usize>>,
+    /// Subtract one when starting work, add one when completing
+    free_capacity: Arc<Mutex<usize>>,
 }
 
+impl LoadShedConf {
+    fn start(&self) -> bool {
+        let mut free_capacity = self.free_capacity.lock().expect("To be able to lock inflight in conf");
+        if *free_capacity > 0 {
+            *free_capacity -= 1;
+            return true;
+        }
+        false
+    }
+
+    fn stop(&self) {
+        *self.free_capacity.lock().unwrap() += 1;
+    }
+}
 
 #[derive(Debug, Clone)]
 pub struct LoadShed<Inner> {
@@ -36,31 +51,64 @@ impl<Inner> LoadShed<Inner> {
                 target,
                 moving_average: Arc::new(Mutex::new(target.as_secs_f64())),
                 ewma_param,
-                inflight: Arc::new(Mutex::new(1))
-            }
+                free_capacity: Arc::new(Mutex::new(1)),
+            },
         }
     }
 }
 
+/// Either an error from the wrapped service or message that the request was shed
+#[derive(Error, Debug)]
+pub enum LoadShedError<T> {
+    #[error("Inner service error")]
+    Inner(#[from] T),
+    #[error("Load shed due to overload")]
+    Overload,
+}
+
 impl<Request, Inner> Service<Request> for LoadShed<Inner>
-where
-    Inner: Service<Request>,
-    Inner::Response: std::fmt::Debug,
-    Inner::Error: std::fmt::Debug,
+    where Inner: Service<Request>,
 {
     type Response = Inner::Response;
-    type Error = Inner::Error;
-    type Future = LoadShedFut<Inner::Future>;
+    type Error = LoadShedError<Inner::Error>;
+    type Future = LoadShedResult<Inner::Future>;
 
     fn poll_ready(&mut self, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
-        self.inner.poll_ready(cx)
+        self.inner.poll_ready(cx).map_err(|err| LoadShedError::Inner(err) )
     }
 
     fn call(&mut self, req: Request) -> Self::Future {
-        LoadShedFut {
-            inner: self.inner.call(req),
-            conf: self.conf.clone(),
-            start: Instant::now()
+        if self.conf.start() {
+            LoadShedResult::Future(
+                LoadShedFut {
+                    inner: self.inner.call(req),
+                    conf: self.conf.clone(),
+                    start: Instant::now(),
+                }
+            )
+        } else {
+            LoadShedResult::Shed
+        }
+    }
+}
+
+/// Calling .project() on a LoadShedResult yields a LoadShedProj
+/// type which has pinned fields
+#[pin_project::pin_project(project=LoadShedProj)]
+pub enum LoadShedResult<Inner> {
+    Future(#[pin] LoadShedFut<Inner>),
+    Shed,
+}
+
+impl<Inner, Output, Error> Future for LoadShedResult<Inner>
+    where Inner: Future<Output=Result<Output,Error>>
+{
+    type Output = Result<Output, LoadShedError<Error>>;
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        match self.project() {
+            LoadShedProj::Future(fut) => fut.poll(cx).map_err(|err| LoadShedError::Inner(err)),
+            LoadShedProj::Shed => Poll::Ready(Err(LoadShedError::Overload)),
         }
     }
 }
@@ -74,9 +122,7 @@ pub struct LoadShedFut<Inner> {
 }
 
 impl<Inner> Future for LoadShedFut<Inner>
-where
-    Inner: Future,
-    Inner::Output: std::fmt::Debug,
+    where Inner: Future,
 {
     type Output = Inner::Output;
 
@@ -95,8 +141,8 @@ where
                 (*moving_average * (1.0 - this.conf.ewma_param)) + (this.conf.ewma_param * elapsed);
             *moving_average
         };
+        this.conf.stop();
         println!("New average: {}", moving_average);
-
         Poll::Ready(result)
     }
 }
